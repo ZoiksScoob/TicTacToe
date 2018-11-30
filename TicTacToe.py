b2ba3bbf4e06302aa0859c9694ac5ea8546cdc6e
Alex <alex_jc_brown@hotmail.com>
2018-11-30 21:26:57 +0000

    Some minor modifications

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,3 @@
-from sys import exit
 def print_board(board):
     print("\n")
     for i, row in enumerate(board):
@@ -19,34 +18,30 @@ def show_rules():
     To specfiy where you want your mark, you need only give the number of the square.
     The numbers are shown below. If you forget, just input "rules" or "r" instead of a number and you'll
     be given this explanation plus the print out below again.
-
-    To quit at any time, input 'q' or 'quit'
     """
     print(rules)
     print_board(rules_board)
 
 
-def is_inp_move(inp, turn, board):
+def check_move_is_valid(num, turn, board):
     input_mapping = {
         "1": (0,0), "2": (0,1), "3": (0,2), 
         "4": (1,0), "5": (1,1), "6": (1,2),
         "7": (2,0), "8": (2,1), "9": (2,2)
         }
 
-    if inp == "q" or inp == "quit":
-        exit(0)
-    elif inp == "r" or inp == "rules":
+    if num == "r" or num == "rules":
         show_rules()
         print_board(board)
         return False
-    elif int(inp) not in range(0,10):
+    elif int(num) not in range(0,10):
         print('This is not a legal move, if you wish to see the rules again just type "rules" or "r"')
         return False
-    elif board[input_mapping[inp][0]][input_mapping[inp][1]] != " ":
+    elif board[input_mapping[num][0]][input_mapping[num][1]] != " ":
         print('This square is already occupied, please select another')
         return False
     else:
-        board[input_mapping[inp][0]][input_mapping[inp][1]] = turn
+        board[input_mapping[num][0]][input_mapping[num][1]] = turn
         return True
 
 
@@ -61,14 +56,22 @@ def next_turn(turn):
 
 def is_game_complete(board):
     # check rows
+    track = 0
+
     for row in board:
         if row[0] != " " and (row[0] == row[1] == row[2]):
             return True
+
+        if {"x", "o"}.issubset(set(row)):
+            track += 1
     
     # check columns
     for i in range(0,3):
         if board[0][i] != " " and (board[0][i] == board[1][i] == board[2][i]):
             return True
+        
+        if {"x", "o"}.issubset(set(board[0][i], board[1][i], board[2][i])):
+            track += 1
 
     # check diagonals
     if board[1][1] != " " and ((board[0][0] == board[1][1] == board[2][2]) or (board[0][2] == board[1][1] == board[2][0])):
@@ -94,7 +97,7 @@ def play_a_game(board, turn):
         print_board(board)
         num = input(f"It's your turn '{turn}', which position?' ")
 
-        if is_inp_move(num, turn, board):
+        if check_move_is_valid(num, turn, board):
             if is_game_complete(board):
                 print_board(board)
                 print(f"The winner is {turn}!")
